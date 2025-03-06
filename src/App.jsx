287b5234a5ec6b287eaed80f0e64f5858d966930
Carlos Folla <karlosf@gmail.com>
2025-03-05 22:17:57 -0300

    Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
 //import { useState } from 'react'
 
 //import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import ItemListContainer from './components/itemlistcontainer';
 import CardWidget from './components/cardwidget';
 
 const App = () => {
   return (
-    <div class="bg-gray-100">
+    <BrowserRouter> 
       <Navbar />
-      <ItemListContainer greeting="Bienvenido a mi tienda!" />
-      <div class="container mx-auto md:flex gap-2 md:flex-wrap grid">
-        <CardWidget imageUrl="https://picsum.photos/seed/10/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
-        <CardWidget imageUrl="https://picsum.photos/seed/9/200/300" title="Compra esto" description="Esto es una descripción" price="10.99"  />
-        <CardWidget imageUrl="https://picsum.photos/seed/8/200/300" title="Producto" description="Esto es una descripción" price="10.99"  />
-        <CardWidget imageUrl="https://picsum.photos/seed/7/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
-        <CardWidget imageUrl="https://picsum.photos/seed/6/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
-        <CardWidget imageUrl="https://picsum.photos/seed/5/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
-        <CardWidget imageUrl="https://picsum.photos/seed/4/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
-        <CardWidget imageUrl="https://picsum.photos/seed/3/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
-      </div>
-    </div>
+      <Routes>
+        <Route path="/" element={<ItemListContainer greeting="Bienvenido a mi tienda!" />} />
+        <Route path="/category/:categoryId" element={<ItemListContainer greeting="Bienvenido a mi tienda!" />} />
+        <Route path="/item/:itemId" element={<CardWidget />} />
+      </Routes>
+    </BrowserRouter>
+        
+
+   
+
+    // <div class="bg-gray-100">
+    //   <ItemListContainer greeting="Bienvenido a mi tienda!" />
+    //   <div class="container mx-auto md:flex gap-2 md:flex-wrap grid">
+    //     <CardWidget imageUrl="https://picsum.photos/seed/10/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
+    //     <CardWidget imageUrl="https://picsum.photos/seed/9/200/300" title="Compra esto" description="Esto es una descripción" price="10.99"  />
+    //     <CardWidget imageUrl="https://picsum.photos/seed/8/200/300" title="Producto" description="Esto es una descripción" price="10.99"  />
+    //     <CardWidget imageUrl="https://picsum.photos/seed/7/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
+    //     <CardWidget imageUrl="https://picsum.photos/seed/6/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
+    //     <CardWidget imageUrl="https://picsum.photos/seed/5/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
+    //     <CardWidget imageUrl="https://picsum.photos/seed/4/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
+    //     <CardWidget imageUrl="https://picsum.photos/seed/3/200/300" title="Mi Card" description="Esto es una descripción" price="10.99"  />
+    //   </div>
+   
+
+    // </div>
   );
 };
 
